3ddeee067a0a18a0d92e5952c3e3be901ff54664
Ramiro Grisales noguera <ramirogrisales@gmail.com>
2022-12-19 14:32:05 -0500

    fix

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const { PORT } = process.env;
 const { dbConnect } = require("./database/db");
 
 app.listen(8080, () => {
-    console.log("servidor corriendo en el puerto ", 8080)
+    console.log("servidor corriendo en el puertdo ", 8080)
     dbConnect();
 });
